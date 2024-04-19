d9eaf6f427b0bf13e7032cbab54c6bf757daff35
Amin Azizzade <amin.azizcomp24@gmail.com>
2024-04-19 11:07:00 +0400

    Uploading Learning Page Resource as ~LearningPageResource~ & ~AppBar~

---
+++ app/src/main/java/com/example/learningassistant/learningPageFragments/learningPageResource/AppBar.kt
@@ -0,0 +1,94 @@
+@file:OptIn(ExperimentalMaterial3Api::class)
+
+package com.example.mylearningasistant4.LearningPageFragments.LearningPageResource
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ButtonDefaults.buttonColors
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+
+private val firstColor = Color.White
+private val secondColor = Color.Unspecified
+private val textColor = Color.Black
+
+
+
+@Composable
+fun AppTopBar(navController: NavController) {
+    TopAppBar(
+        colors = TopAppBarDefaults.topAppBarColors(
+            containerColor = Color.White,
+            titleContentColor = Color.Unspecified,
+            scrolledContainerColor = Color.White
+        ),
+        title = { SpecialMainText() }
+    )
+
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(0.dp, 60.dp, 0.dp, 30.dp),
+        Arrangement.SpaceEvenly,
+        Alignment.CenterVertically
+    )
+    {
+        UpMenuBar(navController = navController)
+    }
+}
+@Composable
+fun UpMenuBar(navController: NavController){
+    Row(verticalAlignment = Alignment.CenterVertically, horizontalArrangement = Arrangement.Center)
+    {
+        SpecialMenuButton(title = "Course", navAddress = "Course", navController = navController)
+        SpecialMenuButton(title = "Private", navAddress = "Private", navController = navController)
+        SpecialMenuButton(title = "Public", navAddress = "Public", navController = navController)
+    }
+
+}
+
+@Composable
+fun SpecialMenuButton(title: String, navAddress: String, navController: NavController){
+
+    Surface(modifier = Modifier)
+    {
+        Button(
+            onClick =
+            {
+                navController.navigate(navAddress)
+            },
+
+            colors = buttonColors(
+                containerColor = Color.White,
+                contentColor = Color.Black,
+                disabledContainerColor = Color.White,
+                disabledContentColor = Color.Black
+            ),
+
+            modifier = Modifier
+                .width(130.dp)
+                .background(Color.White)
+        )
+        {
+            Text(title, fontSize = 20.sp, fontWeight = FontWeight.W400)
+        }
+    }
+}
---
+++ app/src/main/java/com/example/learningassistant/learningPageFragments/learningPageResource/LearningPageResource.kt
@@ -0,0 +1,145 @@
+package com.example.mylearningasistant4.LearningPageFragments.LearningPageResource
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.LinearProgressIndicator
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import com.example.learningassistant.ui.theme.detailColor
+
+val mainColor = Color(113,190,130)
+val notNotificationColor = Color(181, 181, 181)
+
+@Composable
+fun TextIconVertical(size: Int, padding: Int, address: Int, iconColor: Color,
+                     backgroundColor: Color,
+                     navController: NavController?,
+                     nextPage: String,
+                     title: String) {
+    var height = size
+    if (title.isNotEmpty()) height += 50
+    Surface(modifier = Modifier
+        .width(size.dp)
+        .height(height.dp)
+        .padding(padding.dp), color = backgroundColor) {
+        Column {
+            IconButton(onClick = { navController?.navigate(nextPage) }, modifier = Modifier.size(40.dp))
+            {
+                Icon(modifier = Modifier.size(30.dp),
+                    painter = painterResource(id = address),
+                    contentDescription = null,
+                    tint = iconColor,)
+            }
+            Text(text = title, fontSize = 12.sp)
+        }
+
+    }
+}
+@Composable
+fun TextIconHorizontal(width: Int, height: Int, padding: Int, address: Int, iconColor: Color,
+                     backgroundColor: Color,
+                     navController: NavController?,
+                     nextPage: String,
+                     title: String, fontSize: Int, fontWeight: FontWeight, color: Color) {
+    Surface(modifier = Modifier
+        .height(height.dp)
+        .width(width.dp)
+        .padding(padding.dp), color = backgroundColor) {
+        Row {
+            IconButton(onClick = { navController?.navigate(nextPage) }, modifier = Modifier.size(40.dp))
+            {
+                Icon(modifier = Modifier.size(30.dp),
+                    painter = painterResource(id = address),
+                    contentDescription = null,
+                    tint = iconColor,)
+            }
+            Text(text = title, fontSize = fontSize.sp, fontWeight = fontWeight, color = color)
+        }
+
+    }
+}
+
+@Composable
+fun SpecialMainText() {
+    Surface(color = Color.White) {
+        Text(text = "My Learning Groups", textAlign = TextAlign.Center)
+    }
+}
+
+@Composable
+fun LinearProgressBar(totalTask: Int, completedTask: Int){
+    val progress = (completedTask.toFloat()/totalTask)
+    Column(Modifier.padding(5.dp, 0.dp, 0.dp, 0.dp)) {
+        Row {
+            Text(text = "$completedTask",
+                fontSize = 15.sp,
+                fontWeight = FontWeight.SemiBold,
+                color = detailColor)
+            Text(text = "/$totalTask",
+                fontSize = 15.sp,
+                modifier = Modifier.width(60.dp),
+                fontWeight = FontWeight.SemiBold)
+
+            Spacer(modifier = Modifier.padding(80.dp, 0.dp, 0.dp, 0.dp))
+
+            Text(text = (progress*100).toInt().toString()+"%",
+                fontSize = 15.sp,
+                fontWeight = FontWeight.SemiBold,
+                modifier = Modifier.width(60.dp),
+                textAlign = TextAlign.End)
+        }
+
+        Surface {
+            LinearProgressIndicator(
+                progress =  progress ,
+                Modifier
+                    .width(215.dp)
+                    .size(10.dp),
+                color = mainColor
+            )
+        }
+    }
+}
+
+@Composable
+fun PageInterfaceForLearningPages(
+    navController: NavController,
+    navControllerHost: NavController,
+    pageNumber: Int,
+    operation: @Composable () -> Unit
+){
+    Surface(color = Color.Blue) {
+        Scaffold(topBar = { AppTopBar(navController = navController) },
+            bottomBar = { BottomBar(navController = navControllerHost, page = pageNumber) })
+        {
+            Surface(modifier = Modifier
+                .fillMaxSize()
+                .padding(0.dp, 70.dp, 0.dp, 0.dp)
+            )
+            {
+                it.hashCode()
+                operation()
+            }
+        }
+    }
+}
+
