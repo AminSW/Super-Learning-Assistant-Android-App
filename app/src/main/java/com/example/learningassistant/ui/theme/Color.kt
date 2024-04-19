aa1fc7b53ac38fc28929ed93dca235e864795146
Amin Azizzade <amin.azizcomp24@gmail.com>
2024-04-19 11:04:16 +0400

    setting details like colors

--- app/src/main/java/com/example/learningassistant/ui/theme/Color.kt
+++ app/src/main/java/com/example/learningassistant/ui/theme/Color.kt
@@ -1,6 +1,7 @@
 package com.example.learningassistant.ui.theme
 
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.sp
 
 val Purple80 = Color(0xFFD0BCFF)
 val PurpleGrey80 = Color(0xFFCCC2DC)
@@ -9,3 +10,17 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+
+val containerColor = Color(255,255,255)
+val backgroundColorClassic = Color(246, 247, 248)
+val specialIconColor = Color(252, 173, 32)
+val priceColor = Color(72,78,91)
+val detailColor = Color(100,104,116)
+
+val fontSizeMini = 13.sp
+val fontSizeDetails = 15.sp
+val fontSizeHeader = 19.sp
+val fontSizeTopic = 18.sp
+val fontSizeHuge = 20.sp
+val fontSizeMainHeader = 25.sp
