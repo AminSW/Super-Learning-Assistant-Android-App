47d3d366cb64b76531b666c1b968aca6cf8a8428
Amin Azizzade <amin.azizcomp24@gmail.com>
2024-04-19 11:00:00 +0400

    Uploading Learning Pages' rows' code Private, Public and Course one

---
+++ app/src/main/java/com/example/learningassistant/learningPageFragments/learningPageResource/learningPageRows/CourseGroupRow.kt
@@ -0,0 +1,92 @@
+package com.example.mylearningasistant4.LearningPageFragments.LearningPageResource.LearningPageRows
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.learningassistant.learningPageFragments.learningPages.CourseInfoText
+import com.example.learningassistant.R
+import com.example.learningassistant.dataClasses.LearningPageModels.CourseGroupModel
+import com.example.learningassistant.ui.theme.containerColor
+import com.example.learningassistant.ui.theme.detailColor
+
+@Composable
+fun CourseRow(course: CourseGroupModel) {
+    Surface(
+        color = containerColor,
+        modifier = Modifier
+            .padding(10.dp)
+            .height(130.dp),
+        shape = RoundedCornerShape(10.dp),
+        shadowElevation = 3.dp
+    )
+    {
+        Row {
+
+            Surface(
+                modifier = Modifier.padding(10.dp, 15.dp, 10.dp, 15.dp),
+                shape = RoundedCornerShape(15.dp),
+                color = containerColor
+            )
+            {
+                Image(imageVector = ImageVector.vectorResource(id = R.drawable.ic_launcher_background),
+                    contentDescription ="a")
+            }
+
+            Column(
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.Start
+            )
+            {
+                Spacer(modifier = Modifier.padding(0.dp, 5.dp, 0.dp, 0.dp))
+
+                Surface(
+                    modifier = Modifier.height(40.dp)
+                    .fillMaxWidth(),
+                    color = containerColor)
+                {
+                    CourseInfoText(
+                        fontSize    = 17.sp,
+                        info        = course.groupName,
+                        fontWeight  = FontWeight.Bold,
+                        color       = Color.Unspecified
+                    )
+                }
+
+                Surface(
+                    modifier    = Modifier.height(40.dp),
+                    color       = containerColor
+                )
+                {
+                    CourseInfoText(
+                        fontSize    = 14.sp,
+                        info        = course.teacher,
+                        fontWeight  = FontWeight.SemiBold,
+                        color       = detailColor
+                    )
+                }
+                Surface(modifier = Modifier.height(30.dp), color = containerColor) {
+                    LinearProgressBar(course.totalTask, course.completedTask)
+                }
+
+            }
+
+        }
+    }
+
+}
---
+++ app/src/main/java/com/example/learningassistant/learningPageFragments/learningPageResource/learningPageRows/PublicGroupRow.kt
@@ -0,0 +1,79 @@
+package com.example.mylearningasistant4.LearningPageFragments.LearningPageResource.LearningPageRows
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.mylearningasistant4.DataClasses.LearningPageModels.PublicGroupModel
+import com.example.mylearningasistant4.LearningPageFragments.LearningPageResource.TextIconVertical
+import com.example.mylearningasistant4.LearningPageFragments.LearningPageResource.mainColor
+import com.example.mylearningasistant4.LearningPageFragments.LearningPageResource.notNotificationColor
+import com.example.mylearningasistant4.R
+import com.example.mylearningasistant4.containerColor
+
+
+@Composable
+fun PublicGroupsRow(data: PublicGroupModel){
+    val title = data.groupName
+    val message = data.lastMessage
+    val color = if(data.notification) mainColor else notNotificationColor
+    Surface(modifier = Modifier
+        .height(130.dp)
+        .padding(10.dp)
+        .fillMaxWidth(),
+        color = containerColor,
+        shape = RoundedCornerShape(15.dp),
+        shadowElevation = 3.dp
+    ) {
+        Row(horizontalArrangement = Arrangement.SpaceBetween)
+        {
+            Surface(modifier = Modifier.padding(10.dp),
+                shape = RoundedCornerShape(15.dp)
+            ) {
+                Image(painter = painterResource(id = R.drawable.ic_launcher_background),
+                    contentDescription = null)
+            }
+
+
+            Column(modifier = Modifier.height(100.dp),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.SpaceAround) {
+
+                Row(verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.Center)
+                {
+                    Text(text = title, fontSize = 25.sp)
+                }
+                Text(text = message)
+            }
+
+            Column(modifier = Modifier
+                .fillMaxHeight()
+                .width(80.dp),
+                verticalArrangement = Arrangement.SpaceEvenly,
+                horizontalAlignment = Alignment.CenterHorizontally)
+            {
+                TextIconVertical(size = 45,
+                    padding = 8,
+                    address = R.drawable.notification_icon,
+                    iconColor = color,
+                    containerColor, null, "", "")
+            }
+        }
+
+    }
+}
