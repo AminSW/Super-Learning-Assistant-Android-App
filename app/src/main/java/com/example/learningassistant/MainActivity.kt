d7ac686075caa1a2d55a341819f4aca7b4cf2419
Amin Azizzade <amin.azizcomp24@gmail.com>
2024-04-21 09:58:06 +0400

    Uploading main resource codes

--- app/src/main/java/com/example/learningassistant/MainActivity.kt
+++ app/src/main/java/com/example/learningassistant/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.learningassistant.navigations.MainNav
 import com.example.learningassistant.ui.theme.LearningAssistantTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,8 +23,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
-                }
+                    MainNav()                }
             }
         }
     }
@@ -41,6 +41,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     LearningAssistantTheme {
-        Greeting("Android")
+        MainNav()
     }
 }
