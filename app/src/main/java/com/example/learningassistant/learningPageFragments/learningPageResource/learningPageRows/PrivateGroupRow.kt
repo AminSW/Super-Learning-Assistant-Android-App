cd55438e5087bd5fea475e06f0fa6844acd07a81
Amin Azizzade <amin.azizcomp24@gmail.com>
2024-06-21 21:04:19 +0400

    Apply Builder Design Pattern: Applying on PrivateGroupRow

--- app/src/main/java/com/example/learningassistant/learningPageFragments/learningPageResource/learningPageRows/PrivateGroupRow.kt
+++ app/src/main/java/com/example/learningassistant/learningPageFragments/learningPageResource/learningPageRows/PrivateGroupRow.kt
@@ -1,8 +1,6 @@
 package com.example.learningassistant.learningPageFragments.learningPageResource.learningPageRows
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -15,7 +13,6 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.learningassistant.R
@@ -28,55 +25,61 @@ import com.example.learningassistant.ui.theme.containerColor
 
 @Composable
 fun PrivateGroupsRow(data: PrivateGroupModel){
+    val builder = GroupRowBuilder()
     val title = data.groupName
     val color = if(data.notification) mainColor else notNotificationColor
-    Surface(modifier = Modifier
-        .height(130.dp)
-        .padding(10.dp)
-        .fillMaxWidth(),
+    Surface(
+        modifier = Modifier
+            .height(130.dp)
+            .padding(10.dp)
+            .fillMaxWidth(),
         color = containerColor,
         shape = RoundedCornerShape(15.dp),
         shadowElevation = 3.dp
     ) {
         Row(horizontalArrangement = Arrangement.SpaceBetween)
         {
-            Surface(modifier = Modifier.padding(10.dp),
-                shape = RoundedCornerShape(15.dp)
-            ) {
-                Image(painter = painterResource(id = R.drawable.ic_launcher_background),
-                    contentDescription = null)
-            }
+            builder
+                .addImage(Modifier.padding(10.dp), R.drawable.ic_launcher_background)
 
-
-            Column(modifier = Modifier.height(100.dp),
-                horizontalAlignment = Alignment.CenterHorizontally,
-                verticalArrangement = Arrangement.SpaceAround) {
-
-                Row(verticalAlignment = Alignment.CenterVertically,
-                    horizontalArrangement = Arrangement.Center)
-                {
-                    Text(text = title, fontSize = 25.sp)
+                .withText(modifier = Modifier.height(100.dp)){
+                    Row(
+                        verticalAlignment = Alignment.CenterVertically,
+                        horizontalArrangement = Arrangement.Center
+                    ) {
+                        Text(text = title, fontSize = 25.sp)
+                    }
                 }
-            }
+                .addText()
 
-            Column(modifier = Modifier
-                .fillMaxHeight()
-                .width(80.dp),
-                verticalArrangement = Arrangement.SpaceEvenly,
-                horizontalAlignment = Alignment.CenterHorizontally)
-            {
-                TextIconVertical(size = 45,
-                    padding = 8,
-                    address = R.drawable.notification_icon,
-                    iconColor = color,
-                    containerColor, null, "", "")
-                TextIconVertical(size = 45,
-                    padding = 8,
-                    address = R.drawable.entering_way_icon,
-                    iconColor = mainColor,
-                    containerColor, null, "", ""
-                )
-            }
+
+                .withColumnViewHolder(modifier = Modifier.fillMaxHeight().width(80.dp))
+                {
+                    println("ilk")
+                    TextIconVertical(
+                        size = 45,
+                        padding = 8,
+                        address = R.drawable.entering_way_icon,
+                        iconColor = mainColor,
+                        containerColor, null, "", ""
+                    )
+                }
+                .withColumnViewHolder(modifier = Modifier.fillMaxHeight().width(80.dp))
+                {
+                    println("ikinci")
+                    TextIconVertical(
+                        size = 45,
+                        padding = 8,
+                        address = R.drawable.notification_icon,
+                        iconColor = color,
+                        containerColor, null, "", ""
+                    )
+                }
+                .addRecursiveViewHolder()
+
+                .Show()
+
+
         }
 
     }
