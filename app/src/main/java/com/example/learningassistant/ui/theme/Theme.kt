91f1ef677da18f020e11a69421e39fce73f6c105
Amin Azizzade <amin.azizcomp24@gmail.com>
2024-06-25 22:24:17 +0400

    Integrating Main Resource TextUnit classification to Material Theme

--- app/src/main/java/com/example/learningassistant/ui/theme/Theme.kt
+++ app/src/main/java/com/example/learningassistant/ui/theme/Theme.kt
@@ -9,10 +9,14 @@ import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.ReadOnlyComposable
 import androidx.compose.runtime.SideEffect
+import androidx.compose.runtime.compositionLocalOf
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.sp
 import androidx.core.view.WindowCompat
 
 private val DarkColorScheme = darkColorScheme(
@@ -68,3 +72,22 @@ fun LearningAssistantTheme(
         content = content
     )
 }
+
+
+data class FontScheme(
+    val fontSizeMini: TextUnit = 12.sp,
+    val fontSizeDetails: TextUnit = 14.sp,
+    val fontSizeInfo: TextUnit = 17.sp,
+    val fontSizeTopic: TextUnit = 18.sp,
+    val fontSizeHeader: TextUnit = 19.sp,
+    val fontSizeHuge: TextUnit = 20.sp,
+    val fontSizeNews: TextUnit = 22.sp,
+    val fontSizeMainHeader: TextUnit = 25.sp
+)
+
+val LocalFontScheme = compositionLocalOf { FontScheme() }
+
+val MaterialTheme.textUnitScheme: FontScheme
+    @Composable
+    @ReadOnlyComposable
+    get() = LocalFontScheme.current
