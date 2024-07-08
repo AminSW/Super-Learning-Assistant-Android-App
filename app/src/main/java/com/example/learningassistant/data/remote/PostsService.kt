3034415b2ee6dc0cc051fec719b2f0605a60b45f
Amin Azizzade <amin.azizcomp24@gmail.com>
2024-07-08 21:22:39 +0400

    Creating Ktor Clients

---
+++ app/src/main/java/com/example/learningassistant/data/remote/PostsService.kt
@@ -0,0 +1,32 @@
+package com.example.learningassistant.data.remote
+
+import com.example.learningassistant.data.remote.dto.PostRequest
+import com.example.learningassistant.data.remote.dto.PostResponse
+import io.ktor.client.HttpClient
+import io.ktor.client.engine.android.Android
+import io.ktor.client.features.json.JsonFeature
+import io.ktor.client.features.json.serializer.KotlinxSerializer
+import io.ktor.client.features.logging.LogLevel
+import io.ktor.client.features.logging.Logging
+
+interface PostsService {
+
+    suspend fun getPosts(): List<PostResponse>
+
+    suspend fun createPost(postRequest: PostRequest): PostResponse?
+
+    companion object {
+        fun create(): PostsService {
+            return PostsServiceImpl(
+                client = HttpClient(Android) {
+                    install(Logging) {
+                        level = LogLevel.ALL
+                    }
+                    install(JsonFeature) {
+                        serializer = KotlinxSerializer()
+                    }
+                }
+            )
+        }
+    }
+}
