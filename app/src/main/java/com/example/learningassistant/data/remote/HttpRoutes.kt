db4351f75062a3ec300eb06d01fc55390aa24821
Amin Azizzade <amin.azizcomp24@gmail.com>
2024-07-08 21:21:55 +0400

    Creating Ktor data address

---
+++ app/src/main/java/com/example/learningassistant/data/remote/HttpRoutes.kt
@@ -0,0 +1,8 @@
+package com.example.learningassistant.data.remote
+
+object HttpRoutes {
+
+    private const val BASE_URL = "https://jsonplaceholder.typicode.com"
+
+    const val POSTS = "$BASE_URL/posts"
+}
